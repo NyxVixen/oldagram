2de582c40fdc1741538eb91a92743aafd90d90c9
Anastasia Anderson <124330987+NyxVixen@users.noreply.github.com>
2023-06-24 14:06:00 -0600

    Initial upload of files

---
+++ README.md
@@ -0,0 +1,26 @@
+# co0c94792985fa1d872bf45a3
+
+Quick start:
+
+```
+$ npm install
+$ npm start
+````
+
+**Oldagram Project**
+
+Simple project made through the frontend path of Scrimba. Double-click on the picture or heart to "like" the post. Simple as that!
+
+I did add some contrast and remove a bit of brightness from it to hopefully help it stand out more and be less intrusive on the eyes.
+
+```
+At Scrimba our goal is to create the best possible coding school at the cost of a gym membership! 💜
+If we succeed with this, it will give anyone who wants to become a software developer a realistic shot at succeeding, regardless of where they live and the size of their wallets 🎉
+The Frontend Developer Career Path aims to teach you everything you need to become a Junior Developer, or you could take a deep-dive with one of our advanced courses 🚀
+
+- [Our courses](https://scrimba.com/allcourses)
+- [The Frontend Career Path](https://scrimba.com/learn/frontend)
+- [Become a Scrimba Pro member](https://scrimba.com/pricing)
+
+Happy Coding!
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
+
+let likedEl = document.getElementById("liked-el")
+let totalLikes = document.getElementById("total-likes")
+let likeCount = 21492
+
+function likePost() {
+    likeCount += 1
+    let total = (likeCount.toLocaleString("en-US"));
+    totalLikes.innerText = `${total} likes`
+}
+
+
+
